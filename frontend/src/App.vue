4efa26b4f69403d857d8a29f35ee8e1f1d98fe2f
Мухутдинов Искандер <iskanderka125@gmail.com>
2022-03-29 09:42:43 +0300

    Исправляет файлы по настройкам приттера

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,39 @@
 <template>
-    <div>
-        <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
-            <div class="container">
-                <router-link class="navbar-brand float-left" to="/">MEVN</router-link>
-                <ul class="nav navbar-nav flex-row float-right">
-                    <li v-for="item in NAV_ITEM" :key="item.name" class="nav-item mx-1">
-                        <router-link :to="item.to" class="nav-link">{{ item.name }}</router-link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+  <div>
+    <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
+      <div class="container">
+        <router-link class="navbar-brand float-left" to="/">
+          MEVN
+        </router-link>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li v-for="item in NAV_ITEM" :key="item.name" class="nav-item mx-1">
+            <router-link :to="item.to" class="nav-link">
+              {{ item.name }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
 
-        <div class="container mt-5">
-            <router-view></router-view>
-        </div>
+    <div class="container mt-5">
+      <router-view />
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        created() {
-            this.NAV_ITEM = [
-                {
-                    name: 'Create Student',
-                    to: '/student'
-                },
-                {
-                    name: 'View students',
-                    to: '/'
-                }
-            ]
-        },
-    }
+export default {
+  created() {
+    this.NAV_ITEM = [
+      {
+        name: "Create Student",
+        to: "/student",
+      },
+      {
+        name: "View students",
+        to: "/",
+      },
+    ];
+  },
+};
 </script>
--- frontend/src/view/CreatePerson.vue
+++ frontend/src/view/CreatePerson.vue
@@ -1,50 +1,51 @@
 <template>
-    <div class="row justify-content-center">
-        <form-person
-            :person="student"
-            headline="Create Student"
-            action="create"
-            @submit-form="handleSubmitForm" />
-    </div>
+  <div class="row justify-content-center">
+    <form-person
+      :person="student"
+      headline="Create Student"
+      action="create"
+      @submit-form="handleSubmitForm"
+    />
+  </div>
 </template>
 
 <script>
-    import FormPerson from '@/components/FormPerson';
+import FormPerson from "@/components/FormPerson";
 
-    export default {
-        components: {
-            FormPerson,
-        },
+export default {
+  components: {
+    FormPerson,
+  },
 
-        data() {
-            return {
-                student: {
-                    name: '',
-                    email: '',
-                    phone: ''
-                },
-            }
-        },
+  data() {
+    return {
+      student: {
+        name: "",
+        email: "",
+        phone: "",
+      },
+    };
+  },
 
-        methods: {
-            async handleSubmitForm() {
-                let apiURL = 'http://localhost:4000/api/create-student';
+  methods: {
+    async handleSubmitForm() {
+      let apiURL = "http://localhost:4000/api/create-student";
 
-                try {
-                    await fetch(apiURL, {
-                        method: 'POST',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(this.student)
-                    });
+      try {
+        await fetch(apiURL, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(this.student),
+        });
 
-                    await this.$router.push('/')
-                }catch (error) {
-                    console.error(error)
-                }
-            }
-        }
-    }
+        await this.$router.push("/");
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
 </script>
